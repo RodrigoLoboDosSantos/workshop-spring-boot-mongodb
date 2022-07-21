5e4fde196d6fc0baaf272648c6dae420f2d9766d
Rodrigo Lobo dos Santos <lobo_773@hotmail.com>
2022-07-21 18:37:14 -0300

    Database instantiation operation

---
+++ src/main/java/com/rodrigolobodossantos/workshopmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.rodrigolobodossantos.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.rodrigolobodossantos.workshopmongo.domain.Usuario;
+import com.rodrigolobodossantos.workshopmongo.repository.UsuarioRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository usuarioReposiroty;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+
+		usuarioReposiroty.deleteAll();
+
+		Usuario maria = new Usuario(null, "Maria Brown", "maria@gmail.com");
+		Usuario alex = new Usuario(null, "Alex Green", "alex@gmail.com");
+		Usuario bob = new Usuario(null, "Bob Grey", "bob@gmail.com");
+
+		usuarioReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
