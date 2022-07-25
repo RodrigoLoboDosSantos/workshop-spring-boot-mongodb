157dadb784ab6e5a474e96b4a45b709da310b181
Rodrigo Lobo dos Santos <lobo_773@hotmail.com>
2022-07-25 18:28:13 -0300

    User insertion with POST

--- src/main/java/com/rodrigolobodossantos/workshopmongo/services/UsuarioService.java
+++ src/main/java/com/rodrigolobodossantos/workshopmongo/services/UsuarioService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigolobodossantos.workshopmongo.domain.Usuario;
+import com.rodrigolobodossantos.workshopmongo.dto.UsuarioDTO;
 import com.rodrigolobodossantos.workshopmongo.repository.UsuarioRepository;
 import com.rodrigolobodossantos.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UsuarioService {
 	public Usuario findById(String id) {
 		Optional<Usuario> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not founded"));
+	}
+	
+	public Usuario insert(Usuario obj) {
+		return repo.insert(obj);
+	}
+
+	public Usuario fromDTO(UsuarioDTO objDto) {
+		return new Usuario(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
