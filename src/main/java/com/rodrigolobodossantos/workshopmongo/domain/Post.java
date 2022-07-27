941875afabe99d8f3cab0ef689f14294b8e2ff86
Rodrigo Lobo dos Santos <lobo_773@hotmail.com>
2022-07-27 17:32:57 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/rodrigolobodossantos/workshopmongo/domain/Post.java
+++ src/main/java/com/rodrigolobodossantos/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.rodrigolobodossantos.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.rodrigolobodossantos.workshopmongo.dto.AuthorDTO;
+import com.rodrigolobodossantos.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -22,6 +25,8 @@ public class Post implements Serializable {
 	public Post() {
 	}
 
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
