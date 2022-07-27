a64a2c8798ccc42985768e30c072f9c19df1585f
Rodrigo Lobo dos Santos <lobo_773@hotmail.com>
2022-07-27 17:14:38 -0300

    Endpoint to return a user's posts

--- src/main/java/com/rodrigolobodossantos/workshopmongo/resources/UsuarioResource.java
+++ src/main/java/com/rodrigolobodossantos/workshopmongo/resources/UsuarioResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rodrigolobodossantos.workshopmongo.domain.Post;
 import com.rodrigolobodossantos.workshopmongo.domain.Usuario;
 import com.rodrigolobodossantos.workshopmongo.dto.UsuarioDTO;
 import com.rodrigolobodossantos.workshopmongo.services.UsuarioService;
@@ -56,5 +57,11 @@ public class UsuarioResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		Usuario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
